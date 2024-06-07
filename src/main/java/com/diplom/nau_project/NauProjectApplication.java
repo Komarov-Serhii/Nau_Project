60d913040682efc48ee1027d8a5322d945078d9f
Komarov-Serhii <basket779@gmail.com>
2024-06-07 16:40:07 +0200

    NAU

---
+++ src/main/java/com/diplom/nau_project/NauProjectApplication.java
@@ -0,0 +1,13 @@
+package com.diplom.nau_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NauProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NauProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diplom/nau_project/NauProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diplom.nau_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NauProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
